98aa0cdabc76995c2e13f87d257955e19d68db94
Steve Simonson <srsimonson@gmail.com>
2020-06-02 22:48:59 -0500

    framework for inputs

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,6 +5,10 @@ class Contact extends Component {
         return(
             <header>
                 <h2>Contact</h2>
+                <input type="text" placeholder="First Name"></input>
+                <input type="text" placeholder="Last Name"></input>
+                <input type="text" placeholder="Best Ipmression"></input>
+                <button>Join Us!</button>
                     <p>Phone: 555-5555</p>
                     <p>Emnail: fake@email.com</p>
             </header>
